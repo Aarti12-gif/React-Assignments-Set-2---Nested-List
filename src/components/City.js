db0604b96bf5ae8094b4283f94c4c4d6e6bb7ed0
Aarti12-gif <81165320+Aarti12-gif@users.noreply.github.com>
2021-05-30 12:01:44 +0530

    Commit

---
+++ src/components/City.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Town from "./Town";
+function City({name,town,index}){
+    const[clicked,setClicked]=React.useState(false);
+    return(
+        <li>
+        <h4 id={"city" + (index+1)} onClick={()=>setClicked(!clicked)}>
+        {name}
+        </h4>
+        {clicked ?(
+            <ul>
+            {towns.map((item) => (
+                <Town name={item.name} index={towns.indexOf(item)} />
+              ))}
+            </ul>
+          ) : null}
+            
+                </li>
+    );
+}
+export default City;
